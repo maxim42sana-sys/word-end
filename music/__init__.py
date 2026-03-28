3f63515a630415460a6b97070369abeca0ec67cd
Alisa and Max <Children@MacBook-Air-2.local>
2026-03-28 09:29:41 +0300

    Corrected music.Trigger

--- music/__init__.py
+++ music/__init__.py
@@ -2,11 +2,11 @@ from collections import defaultdict as ddict
 
 class Trigger:
   events = ddict(str)
-  def __init__(self, key, event, mus, *args):
+  def __init__(self, key, event, mus, **kwargs):
     self.key = key
     self.event = event
     self.mus = mus
-    self.args = args
+    self.kwargs = kwargs
   def update(self):
     if self.events[self.key] == self.event:
-      self.mus.play(*self.args)
+      self.mus.play(*self.kwargs)
