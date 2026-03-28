7174451adaba51ce6271c5c994b0de9a14cb90bb
Alisa and Max <Children@MacBook-Air-2.local>
2026-03-28 12:35:20 +0300

    Added some mechanics

---
+++ fight/mechanics.py
@@ -0,0 +1,13 @@
+import random
+perc = lambda i: round(i / 100, 2)
+rperc = lambda: perc(random.random() * 100)
+
+def randbool(percents):
+  return rperc() <= percents
+
+class Attack:
+  def __init__(self, strong, *, accurate=perc(90), func="default"):
+    self.accurate = accurate
+    self.strong = strong
+    self.func = func
+  
