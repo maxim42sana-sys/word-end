bd3dca64b54139bd3ab8a257b718a39287902e62
max <maxim42sana@gmail.com>
2026-03-28 16:23:37 +0300

    Update battles.py

--- fight/battles.py
+++ fight/battles.py
@@ -1,3 +1,4 @@
+from copy import copy
 from display import Code, Printer
 import fight.mechanics
 clear = Code('clear code')
@@ -6,5 +7,7 @@ printer = Printer()
 class Player:
   def __init__(self, props='default', lvl=4, *, clr):
     self.lvl = lvl
-    self.props = props if props != 'default' else {'health': 1/2, 'attack': 1/4, 'defence': 1/4} 
+    self.props = props if props != 'default' else {'health': 1/2, 'attack': 1/4, 'defence': 1/4}
     self.get_par = lambda par: self.props[par] * 4 * self.lvl ** 1.1 + 10
+    self.health = copy(self.get_par('health'))
+    self.clr = clr
